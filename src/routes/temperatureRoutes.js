25818fec77a6aee5d2547a6e8790db95b04a8149
SAKTHIVELPG <25it094@drngpit.ac.in>
2026-04-29 21:34:37 +0530

    Update temperatureRoutes.js

--- src/routes/temperatureRoutes.js
+++ src/routes/temperatureRoutes.js
@@ -1,25 +1,51 @@
 const express = require("express");
 const router = express.Router();
 
-let latestTemperature = 26.8;
+let temperatureHistory = [];
 
+const addTemperature = (value) => {
+  const data = {
+    value,
+    time: new Date(),
+  };
+
+  temperatureHistory.push(data);
+
+  if (temperatureHistory.length > 20) {
+    temperatureHistory.shift();
+  }
+};
+
+let latestTemperature = 26.8;
+addTemperature(latestTemperature);
+
+// GET CURRENT + HISTORY
 router.get("/", (req, res) => {
   res.json({
     temperature: latestTemperature,
+    history: temperatureHistory,
     status: "normal",
-    source: "demo",
     timestamp: new Date(),
   });
 });
 
+// UPDATE TEMP
 router.post("/", (req, res) => {
   const { temperature } = req.body;
+
   latestTemperature = temperature;
+  addTemperature(temperature);
+
   res.json({ message: "Temperature updated" });
 });
 
+// SIMULATE SPIKE
 const simulateSpike = (req, res) => {
   latestTemperature = 80;
+  addTemperature(80);
+
+  console.log("⚠️ SPIKE TRIGGERED");
+
   res.json({ message: "Spike simulated" });
 };
 
