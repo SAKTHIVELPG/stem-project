cd9cf19aecf84866e77ad3a92f8715a0a40a9973
SAKTHIVELPG <25it094@drngpit.ac.in>
2026-05-19 22:00:40 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,36 @@
 const express = require("express");
-const cors = require("cors");
-const path = require("path");
 
-const { router: temperatureRoutes, simulateSpike } = require("./routes/temperatureRoutes");
+const cors = require("cors");
+
+const temperatureRoutes =
+require("./routes/temperatureRoutes");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 app.use(cors());
+
 app.use(express.json());
 
-// SERVE WEBSITE
-app.use(express.static(path.join(__dirname, "../public")));
+// 🔥 MAIN ROUTE
 
-app.use("/api/temperature", temperatureRoutes);
-app.get("/api/simulate-spike", simulateSpike);
+app.use(
+  "/api/temperature",
+  temperatureRoutes
+);
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on port ${PORT}`);
+// 🔥 WEBSITE
+
+app.use(
+  express.static("public")
+);
+
+const PORT =
+process.env.PORT || 10000;
+
+app.listen(PORT, () => {
+
+  console.log(
+    `Server running on port ${PORT}`
+  );
+
 });
